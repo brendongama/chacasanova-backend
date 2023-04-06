5ec2b2c657286a54d58a194b19803cc01fae6078
brendongama <brendon_gama@hotmail.com>
2023-04-05 23:57:52 -0300

    teste

--- src/main/java/chacasanova/controllers/PessoaController.java
+++ src/main/java/chacasanova/controllers/PessoaController.java
@@ -43,7 +43,7 @@ public class PessoaController {
 		
 		String content = emailService.getContentMail(pessoaItem.getNome(), pessoaItem.getItem());				
 		try {
-Troc			emailService.sendEmailToCllient("Presente do APARTAMENTO", sendMailThalita, content);
+			emailService.sendEmailToCllient("Presente do APARTAMENTO", sendMailThalita, content);
 		} catch (MessagingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
